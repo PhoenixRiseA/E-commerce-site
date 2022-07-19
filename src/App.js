77d651775da3fc7fc970660b927ff4d5653920f8
John Doe <sailalith1993@gmail.com>
2022-07-19 13:40:55 +0530

    hardcoding items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button";
+const productsArr = [
+  {
+    title: "Colors",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  },
+
+  {
+    title: "Black and white Colors",
+
+    price: 50,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  },
+
+  {
+    title: "Yellow and Black Colors",
+
+    price: 70,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  },
+
+  {
+    title: "Blue Color",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+  },
+];
 
 function App() {
+  const addToCartHandler = () => {
+    console.log("Added to cart");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Products for sale</h1>
+        <p>cart</p>
       </header>
+      <div className="App-items">
+        {productsArr.map((product) => (
+          <div className="App-item">
+            <img src={product.imageUrl} alt={product.title}></img>
+            <div className="item-details">
+              {product.title}: ${product.price}
+              <Button type="submit" onClick={addToCartHandler}>
+                Add to Cart
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
